f754e627010ed21f3a1422a8920449b930029c9c
Ian McCormack <icmccorm@andrew.cmu.edu>
2021-11-08 14:10:58 -0500

    Added script with idea for verifying unsat based on what StarExec will give us.

---
+++ verify_unsat.py
@@ -0,0 +1,3 @@
+# make sure that an output directory is present containing the job output from StarExec.
+# visit every folder in ./output containing a benchmark result that is UNSAT
+# for each benchmark result, look at the file "drat.txt". Make sure all contain 'VERIFIED'.
