8294dc32b320b67c8618bed28d8f13d809b70375
Ian McCormack <icmccorm@andrew.cmu.edu>
2021-11-13 09:34:53 -0500

    Removed drat-trim from scripts and added logging for # sat and unsat.

--- bin/starexec_run_cnftools_mono.sh
+++ bin/starexec_run_cnftools_mono.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 echo "SOLVE"
-./cnftools aux -o 1 $1 | ./cadical $1 ./proof.out
-p=$(./drat-trim $1 ./proof.out -D -v | grep "VERIFIED" | wc | awk '{print  $1}')
-if [ $p -gt 0 ]
-then
-echo "VERIFIED"
-else
-echo "NOT VERIFIED"
-fi
+./cnftools aux -o 1 $1 | ./cadical $1 $2/proof.out
