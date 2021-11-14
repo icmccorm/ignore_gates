08d3af44a2782f56e3fa974c775482860b474449
Chanhee Cho <channy1413@gmail.com>
2021-11-14 12:27:31 -0500

    Implemented under-selecting aux

--- kissat/select_aux.py
+++ kissat/select_aux.py
@@ -1,18 +1,19 @@
 import sys
 
 aux_list = []
-used_clauses = []
+used_literals = []
 
-def add_aux(lit, clauses, gate_type):
-    for c in clauses:
-        for used_c in used_clauses:   # does this work??  int list equivalence?   
-            if c == used_c:
-                return
+def add_aux(lit, literals, gate_type):
+    for l in literals:
+        if l in used_literals:
+            return
 
-    for c in clauses:
-        used_clauses.append(c)
     aux_list.append(lit)
     print(str(lit) + " " + gate_type)
+    for l in literals:
+        used_literals.append(l)
+    
+    used_literals.append(lit)
 
 
 
@@ -20,22 +21,25 @@ fname =sys.argv[1]
 fp = open(fname, "r")
 
 line = fp.readline()
-
 while line:
     # line = lit, gate_type 
     words = line.split()
     lit =  int(words[0])
     gate_type = words[1]
-    clauses = []
+    literals = []
     while True:
         line = fp.readline()
         if "endG" in line:
             break
         clause = [int(x) for x in line.split()]
-        clause = sorted(clause)
-        clauses.append(clause)
-
-    add_aux(lit, clauses, gate_type)
+        for l in clause:
+            if l < 0:
+                l = -l
+            if l != lit and l not in literals:
+                literals.append(l)
+        
+    literals = sorted(literals)
+    add_aux(lit, literals, gate_type)
     line = fp.readline()
 
 
