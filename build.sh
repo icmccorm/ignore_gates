10eadf1f50f159f286f9ad6a72c84c6a195ebe98
Ian McCormack <icmccorm@andrew.cmu.edu>
2021-11-22 12:29:56 -0500

    Added back stats to cadical when running.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 (cd kissat; ./configure && make && cp ./build/kissat ../bin/kissat)
-(cd cadical; ./configure --competition && make && cp ./build/cadical ../bin/cadical)
+(cd cadical; ./configure && make && cp ./build/cadical ../bin/cadical)
 (cd drabt; ./configure.sh && make && cp drabt ../bin/drabt)
 (cp ./kissat/select_aux.py ./bin/select_aux.py)
